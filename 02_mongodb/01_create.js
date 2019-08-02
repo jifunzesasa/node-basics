4143fa6327f398bc45d89719792866840a252735
Alpha Olomi <alphaolomi@gmail.com>
2019-08-02 19:59:04 +0300

    minor edits

--- 02_mongodb/01_create.js
+++ 02_mongodb/01_create.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 const url = "mongodb://localhost:27017/node_basics";
 
-let mongoClient = MongoClient.connect(url,{ useNewUrlParser: true }, { useNewUrlParser: true }, function (err, db) {
+let mongoClient = MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
     if (err) throw err;
     console.log("Database Created!");
     db.close();
